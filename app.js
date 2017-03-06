870741dcbe861d30c397d584203e94167ff0c402
andela-venogwe <victor.enogwe@andela.com>
2017-03-06 16:43:52 +0100

    configure app

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const home = require('./routes/home');
+const search = require('./routes/search');
+const searches = require('./routes/searches');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', home);
+app.use('/api/v1/imagesearch', search);
+app.use('/api/v1/latest/imagesearch', searches);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
